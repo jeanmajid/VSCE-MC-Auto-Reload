eaa6575d9cea5b130e4dc5e00260f40975ed391d
jeanmajid <jeanmajid@users.noreply.github.com>
2024-10-10 14:45:31 +0200

    initial stuff

---
+++ README.md
@@ -0,0 +1,12 @@
+# autoreloadmcpe README
+
+This extension is a simple extension that reloads the current Minecraft: Bedrock Edition addon every time you save a file. This is useful for developers who are working on a Minecraft: Bedrock Edition world and want to see their changes in-game without having to manually reload.
+
+## Requirements
+
+Run /wsserver localhost:8080 in mcpe to connect to the extension.
+
+### 1.0.0
+
+Initial release of autoReloadMCPE
+
---
+++ extension.js
@@ -0,0 +1,108 @@
+const vscode = require("vscode");
+let wss = null;
+let isEnabled = false;
+let connections = [];
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+async function activate(context) {
+    const { Server } = require("ws");
+    const { v4 } = require("uuid");
+
+    const files = await vscode.workspace.findFiles("**/manifest.json", "**/node_modules/**", 1);
+    if (files.length === 0) {
+        return;
+    }
+
+    const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+    statusBarItem.text = "$(debug-start) AutoReload";
+    statusBarItem.tooltip = "Press to start the websocket server";
+    statusBarItem.command = "extension.toggleAutoReload";
+    statusBarItem.show();
+    context.subscriptions.push(statusBarItem);
+
+    const toggleCommand = vscode.commands.registerCommand("extension.toggleAutoReload", async () => {
+        isEnabled = !isEnabled;
+        if (isEnabled) {
+            statusBarItem.text = "$(sync) AutoReload";
+            statusBarItem.tooltip = "Run /wsserver localhost:8080 to connect";
+            wss = new Server({ port: 8080 });
+            wss.on("connection", (ws) => {
+                connections.push(ws);
+                
+                let disposable = vscode.workspace.onDidSaveTextDocument(() => {
+                    sendCommand("/reload", ws, v4());
+                });
+                
+                context.subscriptions.push(disposable);
+                
+                ws.on("close", () => {
+                    disposable.dispose();
+                    const index = connections.indexOf(ws);
+                    if (index > -1) {
+                        connections.splice(index, 1);
+                    }
+                });
+            });
+            await vscode.env.clipboard.writeText("/wsserver localhost:8080");
+            sendTimedNotifaction("wsserver connect command copied to clipboard", 3000);
+        } else {
+            statusBarItem.text = "$(debug-start) AutoReload";
+            statusBarItem.tooltip = "Press to start the websocket server";
+            if (wss) {
+                await new Promise((resolve) => {
+                    connections.forEach((conn) => conn.close());
+                    connections = [];
+                    wss.close(resolve);
+                    wss = null;
+                });
+            }
+        }
+    });
+    context.subscriptions.push(toggleCommand);
+}
+
+function deactivate() {
+    if (wss) {
+        connections.forEach((conn) => conn.close());
+        connections = [];
+        wss.close();
+    }
+}
+
+function sendCommand(command, ws, uuid) {
+    const msg = {
+        header: {
+            version: 1,
+            requestId: uuid,
+            messagePurpose: "commandRequest",
+            messageType: "commandRequest",
+        },
+        body: {
+            version: 1,
+            commandLine: command,
+        },
+    };
+    ws.send(JSON.stringify(msg));
+}
+
+function sendTimedNotifaction(message, time) {
+    vscode.window.withProgress(
+        {
+            location: vscode.ProgressLocation.Notification,
+            title: message,
+            cancellable: false,
+        },
+        () => {
+            return new Promise((resolve) => {
+                setTimeout(resolve, time);
+            });
+        }
+    );
+}
+
+module.exports = {
+    activate,
+    deactivate,
+};
